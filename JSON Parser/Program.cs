bd2b05bf79f10d9824f99078d8eef4ac29510386
Joseph Jordan <joe.jordan1996@gmail.com>
2024-10-25 18:27:18 +0100

    Add project files.

---
+++ JSON Parser/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace JSON_Parser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
